42f49ea0f256e932f27ddf793022a1017590e22f
AlbertoGR5 <111107507+AlbertoGR5@users.noreply.github.com>
2023-04-17 00:29:20 -0500

    Cambios de color

--- test/pruebas.js
+++ test/pruebas.js
@@ -1,23 +1,19 @@
-aquies 
+init 
 
-    definelas 
+    vars 
     
-        digito num = 10 
-        letrillas txt = 'El numero 1 es: ' 
-        digito num2 = 5 
-        letrillas txt1 = 'El numero 2 es: '
+        Num num = 10 
+        String txt = 'El numero 1 es: ' 
 
-    empezando 
+    body
+     
+        log(txt, num) 
+        if (num > 5) {
+            log(numMayor5)
+        }
+        rpt {
+            log(forExample)
+        }
 
-        print(txt, num) 
-        print(txt1, num2) 
-        print(txt, num + num2) 
-/*
-El colibrí abeja de Cuba es la especie de ave más pequeña del mundo, con un tamaño 
-promedio de solo 5 cm y un peso de 2.5 gramos. A pesar de su pequeño tamaño, el colibrí 
-abeja puede volar a velocidades de hasta 80 km/h y puede batir sus alas hasta 80 veces 
-por segundo. Además, su nombre se debe a que su vuelo y zumbido se asemejan al de una 
-abeja. ¡Increíble!
- */
 
-aquifue 
+end
